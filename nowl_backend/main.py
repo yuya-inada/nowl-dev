6187658514ea62cebd21229c9bd2782195fcdaa7
Yuya <obelu1001@gmail.com>
2025-08-12 17:08:46 +0900

    20250812-ReactからFastAPIへの通信がちゃんとできて、APIレスポンスも画面に表示

--- nowl_backend/main.py
+++ nowl_backend/main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# 許可したいオリジン（Reactの開発サーバーURL）を指定
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
